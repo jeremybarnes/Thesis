bf60ff125226d92567c9cfcd92fa853de23e98dd
dosuser <dosuser>
1999-04-03 20:42:49 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@dataset/numsamples.m
@@ -0,0 +1,30 @@
+% numsamples.m
+% Jeremy Barnes, 3/4/1999
+% $Id$
+%
+% NUMSAMPLES - returns the number of observations of a dataset
+%
+% SYNTAX:
+%
+% n = numsamples(obj)
+%
+% RETURNS:
+%
+% The number of observations in a dataset.  Both the SAMPLES and CLASSES
+% methods will return an array which has this number of rows.
+%
+% Basically, it is the size of the dataset.
+
+function n = numsamples(obj)
+
+% PRECONDITIONS:
+% none
+
+n = obj.numsamples;
+
+% POSTCONDITIONS:
+check_invariants(obj);
+
+return;
+
+
