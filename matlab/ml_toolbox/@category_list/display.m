ae06f2ca89901e0db782e2d8a07d169e4f75ae84
dosuser <dosuser>
1999-04-05 00:06:51 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@category_list/display.m
@@ -0,0 +1,13 @@
+function display(obj)
+% DISPLAY method for classlabel
+
+% @classlabel/display.m
+% Jeremy Barnes, 4/4/1999
+% $Id$
+
+disp(' ');
+disp([inputname(1) ' = ']);
+disp(' ');
+disp(obj);
+disp(' ');
+
---
+++ matlab/ml_toolbox/@dataset/display.m
@@ -0,0 +1,12 @@
+function display(obj)
+% DISPLAY method for DATASET
+
+% @dataset/display.m
+% Jeremy Barnes, 4/4/1999
+% $Id$
+
+disp(' ');
+disp([inputname(1) ' = ']);
+disp(' ');
+disp(obj);
+disp(' ');
