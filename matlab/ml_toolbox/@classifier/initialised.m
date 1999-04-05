fe8b220852aeedf9dc9a30947b7f37045dc184fc
dosuser <dosuser>
1999-04-05 12:12:01 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@classifier/initialised.m
@@ -0,0 +1,18 @@
+function i = initialised(obj)
+
+% INITIALISED check that an object has been initialised
+%
+% SYNTAX:
+%
+% i = initialised(obj)
+%
+% RETURNS:
+%
+% I is a boolean which is true iff the object OBJ has been initialised.
+
+% @classifier/initialised.m
+% Jeremy Barnes, 5/4/1999
+% $Id$
+
+
+i = obj.initialised;
