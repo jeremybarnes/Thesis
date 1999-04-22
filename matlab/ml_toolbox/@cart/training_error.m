78b3f0d079dda49a413dd39a5f199678ccf99363
dosuser <dosuser>
1999-04-22 22:18:05 +0000

    Minor bugfixes

--- matlab/ml_toolbox/@cart/training_error.m
+++ matlab/ml_toolbox/@cart/training_error.m
@@ -25,31 +25,4 @@ e = obj.tree.incorrectweight;
 
 
 % POSTCONDITIONS:
-check_invariants(obj);function e = training_error(obj)
-
-% TRAINING_ERROR return the training error of a CART object
-%
-% SYNTAX:
-%
-% e = training_error(obj)
-%
-% RETURNS:
-%
-% The training error is the sum of the weights of samples that are
-% classified incorrectly.  This is returned in E.
-
-% @cart/training_error.m
-% Jeremy Barnes 22/4/1999
-% $Id$
-
-% PRECONDITIONS:
-if (obj.tree.totalweight == 0.0)
-   error('training_error: no weight in samples');
-end
-
-
-e = obj.tree.incorrectweight;
-
-
-% POSTCONDITIONS:
 check_invariants(obj);
