ca4a8ddbd800d37383ecc14f8f1c737b323b1908
dosuser <dosuser>
1999-04-22 21:12:10 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@dataset/x_values.m
@@ -0,0 +1,27 @@
+function x = x_values(obj)
+
+% X_VALUES returns x (independent) variable values of dataset
+%
+% SYNTAX:
+%
+% x = x_values(obj)
+%
+% RETURNS:
+%
+% An array of size (NUMSAMPLES x DIMENSIONS) is returned, which contains
+% all of the independent (x) values of the data, one sample per row.
+
+% samples.m
+% Jeremy Barnes, 3/4/1999
+% $Id$
+
+% PRECONDITIONS:
+% none
+
+x = obj.x_values;
+
+% POSTCONDITIONS:
+check_invariants(obj);
+
+
+
---
+++ matlab/ml_toolbox/@dataset/y_values.m
@@ -0,0 +1,30 @@
+function cat = categories(obj)
+
+% CATEGORIES return the CATEGORY_LIST object of a DATASET
+%
+% SYNTAX:
+%
+% cat = categories(obj)
+%
+% RETURNS:
+%
+% An object of type CATEGORY_LIST which contains the names and number of
+% categories that can appear in the dataset.
+%
+
+% labels.m
+% Jeremy Barnes, 3/4/1999
+% $Id$
+
+
+% PRECONDITIONS:
+% none
+
+
+cat = obj.categories;
+
+
+% POSTCONDITIONS:
+check_invariants(obj);
+
+
