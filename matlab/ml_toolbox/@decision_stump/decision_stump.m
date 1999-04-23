7a543a6530e98af641da2f470149e5286aaca366
dosuser <dosuser>
1999-04-23 19:14:53 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@decision_stump/decision_stump.m
@@ -0,0 +1,65 @@
+function obj = decision_stump(categories, dimensions)
+
+% DECISION_STUMP classifier using a one-split tree algorithm
+%
+% This is the constructor for the decision_stump classifier.
+%
+% SYNTAX:
+%
+% obj = decision_stump(categories, dimensions)
+%
+% Creates a classifier based on the "decision stump" algorithm.  This is
+% a very weak learner, with the primary merit of working quite well in
+% combination with the "boosting" algorithm.
+%
+% This classifier simply splits its data into two sections.  The split is
+% parallel to one of the independent axes of the data.  The split is made
+% to give the overall lowest training risk for the data.
+%
+% Unlike CART, no attempts are made to split the data in a manner that
+% may allow further splits to operate more effectively, as no further
+% splits are ever made.
+%
+% The CATEGORIES parameter is passed a DATASET object that represents the
+% possible values (categories) of the dependent (y) variable.
+%
+% The DIMENSIONS parameter specifies the number of dimensions that the
+% independent (x) value has.
+%
+% RETURNS:
+%
+% OBJ is the template of a decision stump.
+
+% @decision_stump/decision_stump.m
+% Jeremy Barnes, 4/4/1999
+% $Id$
+
+% PRECONDITIONS
+if (dimensions < 1)
+   error('decision_stump: DIMENSIONS must be >= 1');
+elseif (~isa(categories, 'category_list'))
+   error('decision_stump: CATEGORIES must be of the CATEGORY_LIST type');
+end
+
+
+% ancestor relationship
+parent = classifier(categories, dimensions);
+
+% initialisation of variables in obj
+obj.dimensions = dimensions;
+obj.categories = categories;
+
+% Our default split, which splits on the first variable at 0.500
+splitvar = 1;
+splitval = 0.5;
+leftcategory = 0;
+rightcategory = 1;
+
+% construct class and use superior/inferior relationship
+obj = class(obj, 'cart', parent);
+superiorto('double', 'classifier');
+
+
+% POSTCONDITIONS
+check_invariants(obj);
+
