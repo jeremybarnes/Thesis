192c0da968ea17afae8c232059a778b4e67632ad
dosuser <dosuser>
1999-04-25 17:47:22 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@decision_stump/classification_error.m
@@ -0,0 +1,40 @@
+function e = classification_error(obj, varargin)
+
+% CLASSIFICATION_ERROR return the classification error of a DECISION_STUMP
+%
+% SYNTAX:
+%
+% e = classification_error(obj, x, y, [w])
+% e = classification_error(obj, dataset, [w])
+%
+% RETURNS:
+%
+% The training error is the sum of the weights of samples that are
+% classified incorrectly.  This is returned in E.  The {X, Y} parameter
+% can either be specified as a DATASET class or via individual variables.
+%
+% The W vector contains the weight (relative importance) of each data
+% point.  If not specified, it defaults to all ones.  This has the result
+% of E being the number of points that are classified incorrectly.
+
+% @decision_stump/classification_error.m
+% Jeremy Barnes 25/4/1999
+% $Id$
+
+% Get our x, y and w
+[x, y, w] = get_xyw(obj, 'classification_error', varargin);
+
+% Classify the data
+yc = classify(obj, x);
+
+% Find which ones are wrong
+ywrong = (yc ~= y);
+
+% Find the total weight of wrong answers
+weightwrong = sum(ywrong .* w);
+
+% Find the proportion of wrong weight
+e = weightwrong / sum(w);
+
+% POSTCONDITIONS:
+check_invariants(obj);
