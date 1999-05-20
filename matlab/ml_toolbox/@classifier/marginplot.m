009ebe933a7551ed33dd8b22f21b58620299ea65
dosuser <dosuser>
1999-05-20 16:27:42 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@classifier/marginplot.m
@@ -0,0 +1,33 @@
+function marginplot(obj, domain)
+
+% MARGINPLOT generate a plot of the margins of a classifier
+%
+% Domain = [xmin, ymin; xmax, ymax]
+% FIXME: comment
+
+% @classifier/marginplot.m
+% Jeremy Barnes, 20/5/1999
+% $Id$
+
+num_xpoints = 20;
+num_ypoints = 20;
+
+xpoints = linspace(domain(1, 1), domain(2, 1), num_xpoints + 1)';
+ypoints = linspace(domain(1, 2), domain(2, 2), num_ypoints + 1)';
+
+bigm = zeros(num_xpoints+1, num_ypoints+1);
+
+for i=1:num_xpoints+1
+   % Do a row of Y points
+
+   x_data = [ones(num_ypoints+1, 1) .* xpoints(i) ypoints];
+   m = margins(obj, x_data);
+   bigm(:, i) = m;
+end
+
+surf(ypoints, xpoints, bigm+1);
+hold on;
+[c, h] = contour(ypoints, xpoints, bigm+1);
+colorbar;
+shading interp;
+
---
+++ matlab/test/test_margin_plot.m
@@ -0,0 +1,51 @@
+function test_margin_plot
+
+% TEST_MARGIN_PLOT test the margin_plot function
+
+% test_margin_plot.m
+% Jeremy Barnes, 20/5/1999
+% $Id$
+
+b = category_list('binary');
+d = dataset(b, 2);
+data = datagen(d, 'vert', 200, 0, 0);
+
+c = decision_stump(b, 2);
+
+b1 = boost(c, 100);
+trainfirst(b1, data);
+
+for i=1:5
+   figure(1);  clf;
+   marginplot(b1, [0 0; 1 1]);
+   trainagain(b1, data);
+   pause;
+end
+
+function test_margin_plot
+
+% TEST_MARGIN_PLOT test the margin_plot function
+
+% test_margin_plot.m
+% Jeremy Barnes, 20/5/1999
+% $Id$
+
+b = category_list('binary');
+d = dataset(b, 2);
+data = datagen(d, 'ring', 200, 0, 0);
+
+c = decision_stump(b, 2);
+
+b1 = boost(c, 100);
+b1 = trainfirst(b1, data);
+
+for i=1:50
+   figure(1);  clf;
+   marginplot(b1, [0 0; 1 1]);
+   hold on;
+   dataplot(data);
+
+   b1 = trainagain(b1);
+   pause;
+end
+
