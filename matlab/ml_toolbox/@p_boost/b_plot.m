0f17ca4ee715389eeeb1afbd063ed417f190b986
dosuser <dosuser>
1999-05-27 22:07:40 +0000

    * Added nosort option (but not tested) * Added comment

--- matlab/ml_toolbox/@p_boost/b_plot.m
+++ matlab/ml_toolbox/@p_boost/b_plot.m
@@ -1,7 +1,25 @@
-function b_plot(obj, color)
+function b_plot(obj, color, nosort)
 
-% B_PLOT plot the B function of a boost object
-% FIXME: comment
+% B_PLOT plot the B density of a boost object
+% 
+% SYNTAX:
+%
+% b_plot(obj, 'color', ['nosort'])
+%
+% This function plots on the current axes the distribution of the weights
+% B of the weak learner.
+%
+% The plot is made with the colors specified in the COLOR parameter.  If
+% none are specified, then 
+%
+% The weights are sorted first in ascending order, unless the string
+% 'nosort' is passed as the final parameter, in which case the weights
+% remain unsorted.
+%
+% From this plot, an idea of the number of important weak learners can be
+% gained.  A nearly straight line indicates that all are quite important;
+% whereas one that hugs the right of the graph indicates that most of the
+% weight is with a few weak learners.
 
 % @p_boost/b_plot.m
 % Jeremy Barnes, 21/5/1999
@@ -11,8 +29,26 @@ if (nargin == 1)
    color = 'b-';
 end
 
+if (nargin == 2)
+   nosort = [];
+end
+
+if (strcmp(color, 'nosort'))
+   color = 'b-';
+   nosort = 'nosort';
+end
+
+if (~isempty(nosort))
+   if (nosort ~= 'nosort')
+      error(['b_plot: invalid option name "' nosort '".']);
+   end
+end
+
 b = obj.b(1:obj.iterations);
 b = b ./ sum(b);
-b = sort(b);
+
+if (isempty(nosort))
+   b = sort(b);
+end
 
 plot(b, color);
