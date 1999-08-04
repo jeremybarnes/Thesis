a174421c1b4275ef7e31cd45e67737d9cf6a047f
dosuser <dosuser>
1999-08-04 00:11:55 +0000

    Now handles arrays properly

--- matlab/ml_toolbox/@decision_stump/disp.m
+++ matlab/ml_toolbox/@decision_stump/disp.m
@@ -2,11 +2,26 @@ function disp(obj)
 
 % DISP method for DATASET object
 
-% @dataset/disp.m
-% Jeremy Barnes, 4/4/1999
+% @decision_stump/disp.m
+% Jeremy Barnes, 25/4/1999
 % $Id$
 
-disp(['    dataset object:']);
-disp(['      dimensions = ' int2str(obj.dimensions)]);
-disp(['      categories = ' int2str(numcategories(obj.categories))]);
-disp(['      numsamples = ' int2str(obj.numsamples)]);
+s = size(obj);
+if ((s(1) ~= 1) | (s(2) ~= 1))
+   sizestr = ['(' int2str(s(1))];
+   for i=2:length(s)
+      sizestr = [sizestr ' x ' int2str(s(i))];
+   end
+   sizestr = [sizestr ')'];
+   
+   disp(['Array of decision stumps, size = ' sizestr])
+else
+   disp(['    decision stump object:']);
+   disp(['      dimensions    = ' int2str(obj.dimensions)]);
+   disp(['      categories    = ' int2str(numcategories(obj.categories))]);
+   disp(['      splitvar      = ' int2str(obj.splitvar)]);
+   disp(['      splitval      = ' num2str(obj.splitval)]);
+   disp(['      leftcategory  = ' int2str(obj.leftcategory)]);
+   disp(['      rightcategory = ' int2str(obj.rightcategory)]);
+end
+
