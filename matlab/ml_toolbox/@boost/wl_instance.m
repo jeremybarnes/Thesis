5434eb97c194d1a190a965f12864ac067f99f2bf
dosuser <dosuser>
1999-08-06 21:27:22 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@boost/wl_instance.m
@@ -0,0 +1,70 @@
+function wl = wl_instance(obj, range)
+
+% WL_INSTANCE return instance(s) of the weaklearner
+%
+% SYNTAX:
+%
+% wl = wl_instance(obj, range)
+%
+% This function is used to return one or more (trained) instances of the
+% weak learning algorithm.  RANGE indicates for which iterations the weak
+% learning algorithm will be returned.
+%
+% EXAMPLES:
+%
+% wl = wl_instance(obj, 1:iterations(obj));
+%   - returns all instances of the weak learner
+%
+% wl = wl_instance(obj, iterations(obj));
+%   - returns the last one
+%
+
+% @boost/wl_instance.m
+% Jeremy Barnes, 6/8/1999
+% $Id$
+
+if (length(range) == 0)
+   wl = [];
+
+else
+   for i=1:length(range)
+      instance = range(i);
+      wl(i) = obj.classifiers{i}.classifier;
+   end
+end
+
+function wl = wl_instance(obj, range)
+
+% WL_INSTANCE return instance(s) of the weaklearner
+%
+% SYNTAX:
+%
+% wl = wl_instance(obj, range)
+%
+% This function is used to return one or more (trained) instances of the
+% weak learning algorithm.  RANGE indicates for which iterations the weak
+% learning algorithm will be returned.
+%
+% EXAMPLES:
+%
+% wl = wl_instance(obj, 1:iterations(obj));
+%   - returns all instances of the weak learner
+%
+% wl = wl_instance(obj, iterations(obj));
+%   - returns the last one
+%
+
+% @boost/wl_instance.m
+% Jeremy Barnes, 6/8/1999
+% $Id$
+
+if (length(range) == 0)
+   wl = [];
+
+else
+   for i=1:length(range)
+      instance = range(i);
+      wl(i) = obj.classifiers{instance}.classifier;
+   end
+end
+
