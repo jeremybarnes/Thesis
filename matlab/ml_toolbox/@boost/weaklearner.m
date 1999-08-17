decd3d4dc2848d794386080f90b6270d55ff0cee
dosuser <dosuser>
1999-08-17 23:48:18 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@boost/weaklearner.m
@@ -0,0 +1,14 @@
+function wl = weaklearner(obj)
+
+% WEAKLEARNER return the weaklearner upon which a boost is based
+%
+% SYNTAX:
+%
+% wl = weaklearner(obj)
+
+% @boost/weaklearner.m
+% Jeremy Barnes, 17/8/1999
+% $Id$
+
+wl = obj.weaklearner;
+
