75dbe256abf647d286fb03979bee4d47f4fb75f2
dosuser <dosuser>
1999-08-18 20:43:35 +0000

    Fixed up for OOP

--- matlab/ml_toolbox/@classifier/dimensions.m
+++ matlab/ml_toolbox/@classifier/dimensions.m
@@ -1,9 +1,6 @@
-% @classifier/dimensions.m
-% Jeremy Barnes, 4/4/1999
-% $Id$
-% Time-stamp: <1999-04-04 22:28:22 dosuser>
-%
-% DIMENSIONS - returns the number of dimensions of the classifier
+function n = dimensions(obj)
+
+% DIMENSIONS return the number of dimensions of the classifier
 %
 % SYNTAX:
 %
@@ -14,16 +11,9 @@
 % N is the number of dimensions of the independent variable that the
 % classifier OBJ expects.  This is specified in the constructor of the
 % classifier.
-%
 
-function n = dimensions(obj)
-
-% PRECONDITIONS
-% none
+% @classifier/dimensions.m
+% Jeremy Barnes, 4/4/1999
+% $Id$
 
 n = obj.dimensions;
-
-% POSTCONDITIONS
-check_invariants(obj);
-
-return;
