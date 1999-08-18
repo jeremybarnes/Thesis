0e8530b7a5545c891fe6ca1327b0e9110d91f8a6
dosuser <dosuser>
1999-08-18 22:46:56 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@decision_stump/classifier.m
@@ -0,0 +1,10 @@
+function obj_r = classifier(obj)
+
+% CLASSIFIER typecast a DECISION_STUMP as a CLASSIFIER
+
+% @decision_stump/classifier.m
+% Jeremy Barnes, 18/8/1999
+% $Id$
+
+obj_r = obj.classifier;
+
