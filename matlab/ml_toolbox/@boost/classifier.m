5076c05baa67b9661a5408afc4b4670df7816fd3
dosuser <dosuser>
1999-08-18 23:14:51 +0000

    * Now does a proper typecast

--- matlab/ml_toolbox/@boost/classifier.m
+++ matlab/ml_toolbox/@boost/classifier.m
@@ -1,53 +1,9 @@
-function obj = classifier(classes, dimensions)
-% CLASSIFIER abstract ancestor class for a classification machine
-%
-% This is the constructor for the classifier type.
-%
-% SYNTAX:
-%
-% obj = classifier(classes, dimensions)
-%
-% Creates a classifier.  Not much use as this is just an abstract class,
-% anyway.
-%
-% DIMENSIONS specifies the number of dimensions that the independent
-% variable will have.
-%
-% CLASSES is a CLASSLABEL object that specifies the classes to be used in
-% the data.
-%
-% RETURNS:
-%
-% OBJ is the new classifier.
-%
+function obj_r = classifier(obj)
 
-% classifier/classifier.m
-% Jeremy Barnes, 4/4/1999
+% CLASSIFIER typecast a BOOST as a CLASSIFIER
+
+% @decision_stump/classifier.m
+% Jeremy Barnes, 18/8/1999
 % $Id$
 
-
-% PRECONDITIONS
-if (dimensions <= 0)
-   error('classifier: DIMENSIONS must be >= 1');
-end
-
-if (~isa(classes, 'classlabel'))
-   error('classifier: CLASSES needs to be a CLASSLABEL');
-end
-
-
-% initialisation of variables in obj
-obj.initialised = 1;
-obj.dimensions = dimensions;
-obj.classes = classes;
-obj.trained_samples = 0;
-
-% construct class and define superior/inferior relationship
-obj = class(obj, 'classifier');
-superiorto('double');
-
-
-% POSTCONDITIONS
-check_invariants(obj);
-
-return;
+obj_r = obj.classifier;
