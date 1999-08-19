7e26900cab060ba4f93143a4786cc82098c47adb
dosuser <dosuser>
1999-08-19 21:53:19 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@classifier/disp_info.m
@@ -0,0 +1,24 @@
+function disp_info(obj)
+
+% DISP_INFO method for CLASSIFIER object
+
+% @boost/disp.m
+% Jeremy Barnes, 19/8/1999
+% $Id$
+
+disp(['      dimensions    = ' int2str(dimensions(obj))]);
+disp(['      numcategories = ' int2str(numcategories(obj))]);
+function disp_info(obj, inherited)
+
+% DISP_INFO method for CLASSIFIER object
+
+% @boost/disp.m
+% Jeremy Barnes, 19/8/1999
+% $Id$
+
+if (nargin == 2)
+   disp('    --- inherited from classifier:');
+end
+
+disp(['      dimensions    = ' int2str(dimensions(obj))]);
+disp(['      numcategories = ' int2str(numcategories(obj))]);
