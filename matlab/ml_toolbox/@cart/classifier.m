c5af02861cb24fd6685feb0c8ef876d0be7637e2
dosuser <dosuser>
1999-08-19 01:22:11 +0000

    Initial revision

---
+++ matlab/ml_toolbox/@cart/classifier.m
@@ -0,0 +1,11 @@
+function obj_r = classifier(obj)
+
+% CLASSIFIER typecast a DECISION_STUMP as a CLASSIFIER
+
+% @decision_stump/classifier.m
+% Jeremy Barnes, 18/8/1999
+% $Id$
+
+obj = cart(obj);
+obj_r = obj.classifier;
+
