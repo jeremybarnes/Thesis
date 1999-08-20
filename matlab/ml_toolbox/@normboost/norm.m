de6804dc1337fd5c0039e8673a86e61fc2ac4227
dosuser <dosuser>
1999-08-20 11:22:36 +0000

    Fixed

--- matlab/ml_toolbox/@normboost/norm.m
+++ matlab/ml_toolbox/@normboost/norm.m
@@ -10,17 +10,4 @@ function p = norm(obj)
 % Jeremy Barnes, 19/5/1999
 % $Id$
 
-n = obj.p;
-function p = norm(obj)
-
-% NORM return which norm the "normboost" algorithm is optimising
-%
-% SYNTAX:
-%
-% p = norm(obj)
-
-% @normboost/norm.m
-% Jeremy Barnes, 19/5/1999
-% $Id$
-
 p = obj.p;
