68b80edace48d777d4145367c6ea7ec606cca5f7
jeremy <jeremy>
1999-08-23 10:00:45 +0000

    * added default option

--- matlab/ml_toolbox/@boost/boost.m
+++ matlab/ml_toolbox/@boost/boost.m
@@ -31,6 +31,11 @@ if (isa(weaklearner, 'boost'))
    return
 end
 
+% 0 args --> default boost algorithm
+if (nargin == 0)
+   weaklearner = decision_stump;
+end
+
 parent = classifier(categories(weaklearner), dimensions(weaklearner));
 
 % This is a template, which is trained at each boosting iteration
