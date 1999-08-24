d530df2626d8a715b90d981be3bb1c35533b3255
jeremy <jeremy>
1999-08-24 00:09:20 +0000

    * Implemented non-normalised-as-we-go-much-simpler version

--- matlab/ml_toolbox/@normboost2/eval_cf.m
+++ matlab/ml_toolbox/@normboost2/eval_cf.m
@@ -25,7 +25,7 @@ function [cost, dcost, d2cost, margins] = eval_cf(obj, f, alpha)
 %
 % MARG are the margins of H.
 
-% @normboost/eval_cf_deriv.m
+% @normboost2/eval_cf.m
 % Jeremy Barnes, 17/8/1999
 % $Id$
 
@@ -45,42 +45,19 @@ else
    results = ones(size(y_data));
 end
 
-% Use the variables a and b (obtained from alpha) to make our formula
-% simpler to write (see notebook, page 116 for more details).
-p = obj.p;
-beta = (1 - alpha.^p).^(1/p);
-a = alpha .^ p;
-b = 1 - a;
-
 % Calculate margins
 y = y_data*2-1;
 
-margins = beta .* obj.margins + y .* results .* alpha;
+margins = obj.margins + y .* results .* alpha;
 
 % Evaluate sample costs
 sample_costs = exp(-margins);
 
 % Evaluate derivative of sample costs
-d_sample_costs = (b.^(1/p - 1) .* obj.margins ...
-		  - y .* results .* a.^(1/p-1)) ...
-                 .* sample_costs;
+d_sample_costs = -y .* results .* sample_costs;
 
 % Second derivative of sample costs
-%d2_sample_costs = (b.^(1/p - 2) .* (p*(1 + b) - 1) .* obj.margins ...
-%		   + a.^(1/p - 2) .* (p*(1 - a) - 1) .* y .* results) ...
-%                  .* sample_costs;
-
-% Last method gave same answer as d_sample_costs, this one is more
-% explicit and hopefully more right...
-
-F_bit = b^(1/p) .* obj.margins;
-f_bit = y .* a^(1/p) .* results;
-
-factor_1 = F_bit .* (1/(p * b.^2) - 1/(p.^2 * b.^2));
-factor_2 = f_bit .* (1/(p*a.^2) - 1/(p.^2 * a.^2));
-factor_3 = (F_bit ./ (p*b) - f_bit ./ (p*a)).^2;
-d2_sample_costs = (factor_1 + factor_2 + factor_3) .* sample_costs;
-
+d2_sample_costs = y.^2 .* results.^2 .* sample_costs;
 
 % Add up to get totals
 cost = sum(sample_costs);
