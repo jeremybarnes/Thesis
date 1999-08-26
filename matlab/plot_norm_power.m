a7e7113a3085b7bdc056498293b8a939e87df301
jeremy <jeremy>
1999-08-26 18:52:34 +0000

    This file plots the diagram on page 135 of my notebook

---
+++ matlab/plot_norm_power.m
@@ -0,0 +1,37 @@
+function plot_norm_power
+
+% plot_norm_power.m
+% Jeremy Barnes, 26/8/1999
+% $Id$
+
+% This generates a plot of the "normalising power" (see page 135 of my
+% notebook) for different values of p.
+
+gamma = logspace(-3, 2);
+
+all_p = [0.5 1.0 2.0];
+all_colors = 'brk';
+
+figure(1);  clf;
+
+for i=1:length(all_p)
+   p = all_p(i);
+   color = all_colors(i);
+   
+   norm_power = calc_norm_power(gamma, p);
+   
+   semilogx(gamma, norm_power, [color '-']);  hold on;
+end
+
+xlabel('gamma');
+ylabel('norm_power');
+grid on;
+legend('p=0.5', 'p=1.0', 'p=2.0', 0);
+   
+   
+
+
+function norm_power = calc_norm_power(gamma, p)
+
+norm_power = 1 ./ (1 + gamma.^p).^(1/p);
+
