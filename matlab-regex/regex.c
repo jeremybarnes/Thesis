978a5ca84433e63f5384ed3c37979f0cb39f781f
jeremy <jeremy>
1999-09-10 13:04:45 +0000

    * Put fixed structures on the stack instead of the heap for flexibility * Fixed some minor errors * Started implementing more structured option handling * Hangs at regfree for some reason

--- matlab-regex/regex.c
+++ matlab-regex/regex.c
@@ -35,7 +35,7 @@ mxArray_t *reg_to_matlab_string(char *s, struct re_registers *registers,
     }
 
     /* Find out the size of the string */
-    reg_size = (registers->end[num] - registers->start[num] + 1);
+    reg_size = (registers->end[num] - registers->start[num]);
 
     /* Allocate, +1 for terminator */
     reg_str = mxCalloc(reg_size+1, sizeof(char));
@@ -48,12 +48,15 @@ mxArray_t *reg_to_matlab_string(char *s, struct re_registers *registers,
     /* Copy it in and add null terminator */
     strncpy(reg_str, s + (registers->start[num]), reg_size);
     reg_str[reg_size+1] = '\0';
+    mexPrintf("Register number %d has string \"%s\"\n", num, reg_str);
     out = mxCreateString(reg_str);
     if (out == NULL)
 	mexErrMsgTxt("regex: unable to allocate memory for reg array");
     mxFree(reg_str);
     return out;
 }
+
+enum reg_output_type { ROT_NONE, ROT_CELL, ROT_INDIVIDUAL };
 
 
 void mexFunction(int nlhs, mxArray_t *plhs[],
@@ -65,8 +68,9 @@ void mexFunction(int nlhs, mxArray_t *plhs[],
     char *pattern_str, *string_str;
     int res, individual = 0, boolean = 0, pattern_len, string_len, num_regs, i;
     const char *error_msg;
-    struct re_registers *registers;
+    struct re_registers registers;
     struct re_pattern_buffer pattern_buf;
+    char fastmap[FASTMAP_SIZE];
 
     /* No output arguments: just return */
     if (nlhs == 0) return;
@@ -100,6 +104,8 @@ void mexFunction(int nlhs, mxArray_t *plhs[],
     if (res != 0)
 	mexErrMsgTxt("regex: pattern was not a string");
 
+    mexPrintf("pattern_str (length %d) is \"%s\"\n", pattern_len, pattern_str);
+
     /* Get the string to match */
     string_len = mxGetNumberOfElements(string);
     string_str = mxCalloc(string_len+1, sizeof(char));
@@ -109,42 +115,51 @@ void mexFunction(int nlhs, mxArray_t *plhs[],
     res = mxGetString(string, string_str, string_len+1);
     if (res != 0) mexErrMsgTxt("regex: second argument was not a string");
 
-    /* Allocate our fastmap */
-    pattern_buf.fastmap = mxCalloc(FASTMAP_SIZE, sizeof(char));
-    if (pattern_buf.fastmap == NULL)
-	mexErrMsgTxt("regex: unable to allocate memory for a fastmap");
+    mexPrintf("string_str (length %d) is \"%s\"\n", string_len, string_str);
 
     /* Set up our regexp buffer */
     pattern_buf.translate = NULL;
     pattern_buf.buffer = NULL;
     pattern_buf.allocated = 0;
     pattern_buf.regs_allocated = REGS_UNALLOCATED;
+    pattern_buf.fastmap = fastmap;
+
+    mexPrintf("Compiling regular expression...");
 
     /* Compile the regular expression */
     error_msg = re_compile_pattern(pattern_str, pattern_len, &pattern_buf);
 
     if (error_msg != NULL) mexErrMsgTxt(error_msg);
 
+    mexPrintf("done.\n");
+
+    mexPrintf("Searching regular expression...");
+
     /* Do a search on the regular expression */
     res = re_search(&pattern_buf, string_str, string_len, 0, string_len,
-		    registers);
+		    &registers);
 
     if (res == -2) mexErrMsgTxt("regex: internal error in matcher!");
+
+    mexPrintf("done.\n");
 
     if (boolean) {
 	matched = mxCreateDoubleMatrix(1, 1, mxREAL);
 	if (matched == NULL)
 	    mexErrMsgTxt("regex: unable to allocate matched array");
 	
-	if (res == -1) /* Not found */
+	if (res == -1) { /* Not found */
 	    *mxGetPr(matched) = 0.0;
-	else  /* Found */
+	    mexPrintf("Match was not found\n");
+	} else { /* Found */
 	    *mxGetPr(matched) = 1.0;
+	    mexPrintf("Match was found\n");
+	}
 	plhs[0] = matched;
     }
     
     else {
-	matched = reg_to_matlab_string(string_str, registers, 0);
+	matched = reg_to_matlab_string(string_str, &registers, 0);
 	plhs[0] = matched;
     }	
 
@@ -153,16 +168,18 @@ void mexFunction(int nlhs, mxArray_t *plhs[],
 
     if (individual) num_regs = nlhs;
     else {
-	num_regs = registers->num_regs;
+	num_regs = pattern_buf.re_nsub;
 	cells = mxCreateCellMatrix(num_regs, 1);
 	if (cells == NULL)
 	    mexErrMsgTxt("regex: unable to create output cell array");
     }
 
+    mexPrintf("Number of output regs = %d\n", num_regs);
+
     for (i=0; i<num_regs; i++) {
 	mxArray_t *out;
 
-	out = reg_to_matlab_string(string_str, registers, i);
+	out = reg_to_matlab_string(string_str, &registers, i);
 	if (out == NULL)
 	    mexErrMsgTxt("regex: couldn't get register");
 
@@ -174,11 +191,16 @@ void mexFunction(int nlhs, mxArray_t *plhs[],
 	plhs[1] = cells;
 
     /* Clean up */
-    mxFree(pattern_buf.fastmap);
+    mexPrintf("Cleaning up registers...");
+    mxFree(registers.start);
+    mxFree(registers.end);
+    mexPrintf("done.\nCleaning up pattern_buf...");
     pattern_buf.fastmap = NULL;
     regfree(&pattern_buf);
+    mexPrintf("done.\nCleaning up strings...");
     mxFree(string_str);
     mxFree(pattern_str);
+    mexPrintf("done.\n");
 }    
     
     
